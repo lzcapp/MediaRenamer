3d561ee7bed2e1cb02994c76ae8714e2a86d5587
Zichen Luo <iluo@163.com>
2019-10-26 17:46:08 +0800

    Rebuild MediaRenamer project
    1. Code cleanup and reformat
    2. Remove ExportGPS function and it's now only a renaming tool.

---
+++ MediaRenamer/FileHandle.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using static MediaRenamer.MetadataQuery;
+using static MediaRenamer.Program;
+
+namespace MediaRenamer {
+    public static class FileHandle {
+        internal static void FileProcess(FileSystemInfo file) {
+            try {
+                string strDt;
+                var fileExt = file.Extension.ToLower();
+                if (PicExt.Contains(fileExt)) {
+                    var dictResult = MetaQuery(file, true);
+                    strDt = dictResult["datetime"];
+                    if (string.IsNullOrEmpty(strDt)) {
+                        return;
+                    }
+
+                    Rename(file, string.IsNullOrEmpty(strDt) ? "ERROR" : strDt);
+                } else if (VidExt.Contains(fileExt)) {
+                    var dictResult = MetaQuery(file, false);
+                    strDt = dictResult["datetime"];
+                    if (string.IsNullOrEmpty(strDt)) {
+                        return;
+                    }
+
+                    Rename(file, string.IsNullOrEmpty(strDt) ? "ERROR" : strDt);
+                } else {
+                    var dictResult = MetaQuery(file, true);
+                    strDt = dictResult["datetime"];
+                    if (!string.IsNullOrEmpty(strDt)) {
+                        dictResult = MetaQuery(file, false);
+                        strDt = dictResult["datetime"];
+                    }
+
+                    if (string.IsNullOrEmpty(strDt)) {
+                    }
+                }
+            } catch (Exception) {
+                // ignored
+            }
+        }
+
+        private static void Rename(FileSystemInfo file, string strDt) {
+            switch (strDt) {
+                case "ERROR":
+                    Console.WriteLine("[-Error-] " + file.FullName + " --> NO Tag Found");
+                    return;
+                case "ERROR Unknown Type":
+                    Console.WriteLine("[-Error-] " + file.FullName + " --> UNKOWN File Type");
+                    return;
+                default:
+                    var strOriginName = file.FullName;
+                    var strPath = file.FullName.Replace(file.Name, "");
+                    var strFullName = Path.Combine(strPath, strDt);
+                    var strExt = file.Extension.ToLower();
+                    var intDupInx = 0;
+                    var strDupName = strFullName + strExt;
+                    if (File.Exists(strDupName) && FileCompare(strDupName, strOriginName)) {
+                        Console.WriteLine("[Skipped] " + file.FullName);
+                        return;
+                    }
+
+                    while (File.Exists(strDupName)) {
+                        intDupInx++;
+                        var strDupInx = "_" + intDupInx;
+                        strDupName = strFullName + strDupInx + strExt;
+                    }
+
+                    var strNewName = strDupName;
+                    try {
+                        var fileInfo = new FileInfo(strOriginName);
+                        fileInfo.MoveTo(strNewName);
+                        Console.WriteLine("[-Moved-] " + " --> " + strNewName);
+                    } catch (Exception) {
+                        Console.WriteLine("[-Error-] " + " --> Error rename");
+                    }
+
+                    return;
+            }
+        }
+
+        private static bool FileCompare(string filePath1, string filePath2) {
+            using (var hash = HashAlgorithm.Create()) {
+                using (FileStream file1 = new FileStream(filePath1, FileMode.Open),
+                                  file2 = new FileStream(filePath2, FileMode.Open)) {
+                    var hashByte1 = hash.ComputeHash(file1);
+                    var hashByte2 = hash.ComputeHash(file2);
+                    var fileByte1 = BitConverter.ToString(hashByte1);
+                    var fileByte2 = BitConverter.ToString(hashByte2);
+                    return fileByte1 == fileByte2;
+                }
+            }
+        }
+    }
+}
---
+++ MediaRenamer/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using static MediaRenamer.FileHandle;
+
+namespace MediaRenamer {
+    internal static class Program {
+        public static readonly List<string> VidExt = new List<string> {".mp4", ".mov", ".mts", ".mt2s"};
+        public static readonly List<string> PicExt = new List<string> {".dng", ".jpg", ".jpeg"};
+        // ReSharper disable once FieldCanBeMadeReadOnly.Global
+
+        private static void Main() {
+            Console.WriteLine("Copyright © 2019 RainySummer, All Rights Reserved.");
+            Console.WriteLine(">> Tool for Renaming multi-media files.\n");
+
+            Console.WriteLine(@"Please Input the Folder Path:");
+            Console.WriteLine(@"[d] for default folder (E:\DCIM\)");
+            string dirInput;
+            do {
+                dirInput = Console.ReadLine();
+            } while (string.IsNullOrWhiteSpace(dirInput));
+
+            var isDefaultDir = dirInput == "d";
+
+            try {
+                var diDefault = new DirectoryInfo(@"D:\DCIM\");
+                var diPath = isDefaultDir ? diDefault : new DirectoryInfo(dirInput);
+
+                var fileList = new List<FileSystemInfo>();
+                fileList = GetFiles(diPath, fileList);
+                foreach (var file in fileList) {
+                    FileProcess(file);
+                }
+            } catch (DirectoryNotFoundException exDirNotFound) {
+                Console.WriteLine("ERR: The folder does not exist. [Program Exiting...]");
+            } catch (Exception ex) {
+                Console.WriteLine("ERR: " + ex.Message + " [Program Exiting...]");
+            }
+        }
+
+        private static List<FileSystemInfo> GetFiles(DirectoryInfo dirInfo, List<FileSystemInfo> fileList) {
+            var fsInfos = dirInfo.GetFileSystemInfos();
+            foreach (var fsInfo in fsInfos) {
+                if (fsInfo is DirectoryInfo) {
+                    GetFiles(new DirectoryInfo(fsInfo.FullName), fileList);
+                } else {
+                    var fileExt = fsInfo.Extension.ToLower();
+                    if (PicExt.Contains(fileExt) || VidExt.Contains(fileExt)) {
+                        fileList.Add(fsInfo);
+                    }
+                }
+            }
+
+            return fileList;
+        }
+    }
+}
