7104545391c84eb1fb0f53104b3c3e65d9cdba4c
Laurence Luo <iluo@163.com>
2020-07-03 13:04:34 +0800

    New project

---
+++ ExifViewer/Program.cs
@@ -0,0 +1,26 @@
+﻿using MetadataExtractor;
+using System;
+
+namespace ExifViewer {
+    class Program {
+        static void Main() {
+            string path1 = @"F:\2053428.jpg";
+
+            var directories = ImageMetadataReader.ReadMetadata(path1);
+
+            foreach (var directory in directories) {
+                foreach (var tag in directory.Tags) {
+                    Console.WriteLine($"[{directory.Name}] {tag.Name} = {tag.Description}");
+                }
+
+                if (directory.HasError) {
+                    foreach (var error in directory.Errors) {
+                        Console.WriteLine($"ERROR: {error}");
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
