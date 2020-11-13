52b3cfcb604f39fd44475108dc12c23c38128fc7
Laurence Luo <iluo@163.com>
2020-11-13 04:20:46 +0000

    Add MediaInfo() to ExifViewer fot debugging

--- ExifViewer/Program.cs
+++ ExifViewer/Program.cs
@@ -1,12 +1,21 @@
-﻿using MetadataExtractor;
+﻿using MediaInfoLib;
+using MetadataExtractor;
 using System;
+using System.IO;
 
 namespace ExifViewer {
     class Program {
         static void Main() {
-            string path1 = @"F:\2053428.jpg";
+            string path = "";
 
-            var directories = ImageMetadataReader.ReadMetadata(path1);
+            var directories = ImageMetadataReader.ReadMetadata(path);
+
+            FileSystemInfo file = new FileInfo(path);
+            var mi = new MediaInfo();
+            mi.Open(file.FullName);
+            Console.WriteLine(mi.Inform());
+            mi.Close();
+            mi.Dispose();
 
             foreach (var directory in directories) {
                 foreach (var tag in directory.Tags) {
