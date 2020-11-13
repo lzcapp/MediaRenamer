8563c1a7415155b4479cadd006addd95693850c7
Laurence Luo <iluo@163.com>
2020-11-13 05:00:02 +0000

    Avoid crash when querying videos with 1970-1-1 encode date

--- MediaRenamer/MetadataQuery.cs
+++ MediaRenamer/MetadataQuery.cs
@@ -58,6 +58,9 @@ namespace MediaRenamer {
                 var dtDt = DateTime.ParseExact(strDt, "yyyy:MM:dd HH:mm:ss", CultureInfo.CurrentCulture);
                 var startTime = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1, 0, 0, 0, 0));
                 var timestamp = (dtDt.Ticks - startTime.Ticks) / 10000;
+                if (timestamp == 0) {
+                    return null;
+                }
                 var dictDt = new Dictionary<string, string> {
                     {"datetime", dtDt.ToString(strDtFormat)},
                     {"timestamp", timestamp + ""}
@@ -85,9 +88,8 @@ namespace MediaRenamer {
                 if (string.IsNullOrEmpty(strDt)) {
                     strDt = mi.Get(StreamKind.General, 0, "Recorded_Date");
                 }
-                mi.Close();
             } catch (Exception) {
-                mi.Close();
+                mi.Dispose();
                 return null;
             }
             mi.Dispose();
@@ -95,18 +97,24 @@ namespace MediaRenamer {
             string strSourceDtFormat;
             if (isApple) {
                 strSourceDtFormat = "yyyy-MM-ddTHH:mm:ss";
-                var strTz = strDt.Substring(19);
-                strDt = strDt.Replace(strTz, "");
+                if (strDt.Length > 19) {
+                    var strTz = strDt.Substring(19);
+                    strDt = strDt.Replace(strTz, "");
+                }
             } else {
                 strSourceDtFormat = "yyyy-MM-dd HH:mm:ss";
-                var strTz = strDt.Substring(0, 3);
-                strDt = strDt.Replace(strTz + " ", "");
+                if (strDt.Length > 19) {
+                    var strTz = strDt.Substring(0, 3);
+                    strDt = strDt.Replace(strTz + " ", "");
+                }
             }
             var dtDt = DateTime.ParseExact(strDt, strSourceDtFormat, CultureInfo.CurrentCulture);
             dtDt = ConvertTimeFromUtc(dtDt, Local);
             var startTime = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1, 0, 0, 0, 0));
             var timestamp = (dtDt.Ticks - startTime.Ticks) / 10000;
-
+            if (timestamp == 0) {
+                return null;
+            }
             const string strDtFormat = "yyyy.MM.dd_HHmmss";
             var dictDt = new Dictionary<string, string> {
                 {"datetime", dtDt.ToString(strDtFormat)},
