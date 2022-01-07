7caf70d6e336cc2faca27933d818df15696c0c42
Laurence Luo <iluo@163.com>
2022-01-07 16:51:45 +0800

    Update FileHandle.cs

--- FileHandle.cs
+++ FileHandle.cs
@@ -5,27 +5,17 @@ namespace MediaRenamer {
     public static class FileHandle {
         internal static bool FileProcess(FileSystemInfo file) {
             try {
-                var dictResult = MetaQuery(file, true);
-
-                if (dictResult == null || dictResult.ContainsKey("error")) {
-                    dictResult = null;
-                    dictResult = MetaQuery(file, false);
-                }
-
-                if (dictResult == null) {
-                    Console.WriteLine("[-Error-] MetaQuery returns NULL result: " + file.FullName + ".");
-                    return false;
-                }
+                var dictResult = MetaQuery(file);
 
                 if (dictResult.ContainsKey("error")) {
-                    Console.WriteLine("[-Error-] MetaQuery returns ERROR: " + file.FullName + ".");
+                    Console.WriteLine("[-Error-] MetaQuery Error: " + file.FullName + ".");
                     return false;
                 }
 
                 string strDt = dictResult["datetime"];
                 Rename(file, strDt);
             } catch (Exception ex) {
-                Console.WriteLine("[-Error-] FileProcess error: " + ex.Message + ".");
+                Console.WriteLine("[-Error-] FileProcess Failed: " + file.FullName + " | " + ex.Message + ".");
                 return false;
             }
 
@@ -33,46 +23,31 @@ namespace MediaRenamer {
         }
 
         private static void Rename(FileSystemInfo file, string strDt) {
-            var strOriginName = file.FullName;
             var strPath = file.FullName.Replace(file.Name, "");
             var strFullName = Path.Combine(strPath, strDt);
             var strExt = file.Extension.ToLower();
-            var intDupInx = 0;
-            var strDupName = strFullName + strExt;
+            var strMD5 = "";
 
-            var fileInfo = new FileInfo(strOriginName);
-
-            while (File.Exists(strDupName)) {
-                if (FileCompare(strDupName, strOriginName)) {
-                    fileInfo.Delete();
-                    Console.WriteLine("[Skipped] " + file.FullName);
-                    return;
+            using (var md5Instance = MD5.Create()) {
+                using (var stream = File.OpenRead(file.FullName)) {
+                    var fileHash = md5Instance.ComputeHash(stream);
+                    strMD5 = BitConverter.ToString(fileHash).Replace("-", "").ToLowerInvariant();
                 }
-                intDupInx++;
-                strDupName = strFullName + "_" + intDupInx + strExt;
             }
 
-            var strNewName = strDupName;
+            var strOutName = strFullName + "_" + strMD5 + strExt;
+
+            var fileInfo = new FileInfo(file.FullName);
+            if (File.Exists(strOutName)) {
+                fileInfo.Delete();
+                return;
+            }
+
             try {
-                fileInfo.MoveTo(strNewName);
-                Console.WriteLine("[-Moved-] " + strNewName);
-            } catch (Exception) {
-                Console.WriteLine("[-Error-] Error renaming");
-            }
-        }
-
-        private static bool FileCompare(string filePath1, string filePath2) {
-            if (filePath1 == filePath2) {
-                return true;
-            }
-
-            using (var hash = HashAlgorithm.Create()) {
-                using (FileStream file1 = new(filePath1, FileMode.Open),
-                                  file2 = new(filePath2, FileMode.Open)) {
-                    var hashByte1 = hash.ComputeHash(file1);
-                    var hashByte2 = hash.ComputeHash(file2);
-                    return hashByte1 == hashByte2;
-                }
+                fileInfo.MoveTo(strOutName);
+                Console.WriteLine("[-Moved-] " + strOutName);
+            } catch (Exception ex) {
+                Console.WriteLine("[-Error-] Rename Error: " + ex.Message);
             }
         }
     }
